b5e43fd8f7a1166aaa8bd612d4e395cd61b26c37
HBO <ogurhale@gmail.com>
2022-06-11 21:50:36 +0300

    Add files via upload

---
+++ Scripts/Scenario-I/PT_CVE_2021_26086/PT_CVE_2021_26086_Attacker1/attackerController.py
@@ -0,0 +1,37 @@
+from attackerBase import *
+
+# Import your custom attack classes here
+
+from PT_CVE_2021_26086_Attacker1 import PT_CVE_2021_26086_Attacker1
+
+
+import time
+import random
+
+def attackInvoker(attackerBase: AttackerBase):
+    print("Invoked attack " + attackerBase.__class__.__name__)
+    attackerBase.templateMethod()
+    print("Finished attack "+ attackerBase.__class__.__name__)
+
+if __name__ == '__main__':
+    attacks = [PT_CVE_2021_26086_Attacker1()]
+    # Call your custom attacks here
+
+    tic = time.time()
+
+    for attack in attacks:
+        attackInvoker(attack)
+        time.sleep(random.randint(5,8))
+
+    toc = time.time()
+
+    print("Total elapsed time: ", toc - tic)
+    
+
+    
+
+
+
+
+
+
