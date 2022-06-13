ddd03fde9516936054463e18a726dba2c77091f0
HBO <ogurhale@gmail.com>
2022-06-13 11:06:50 +0300

    Update serialAttackerController.py

--- Scripts/Scenario-II/OSCI_CVE_2019_15107/serialAttackerController.py
+++ Scripts/Scenario-II/OSCI_CVE_2019_15107/serialAttackerController.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     # Call your custom attacks here
 
     tic = time.time()
-    indices = [x for x in range(attacks)]
+    indices = [x for x in range(len(attacks))]
 
     while len(indices):
         i = random.randrange(len(indices))
