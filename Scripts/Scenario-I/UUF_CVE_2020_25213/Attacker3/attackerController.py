d07a0283848edb0dafb1893854294c413243b787
HBO <ogurhale@gmail.com>
2022-06-15 12:49:51 +0300

    Add files via upload

---
+++ Scripts/Scenario-I/UUF_CVE_2020_25213/Attacker3/attackerController.py
@@ -0,0 +1,32 @@
+from attackerBase import *
+
+# Import your custom attack classes here
+from UUF_CVE_2020_25213_Attacker3 import UUF_CVE_2020_25213_Attacker3
+
+import time
+import random
+
+def attackInvoker(attackerBase: AttackerBase):
+    print("Invoked attack " + attackerBase.__class__.__name__)
+    attackerBase.templateMethod()
+    print("Finished attack "+ attackerBase.__class__.__name__)
+
+if __name__ == '__main__':
+    attacks = [UUF_CVE_2020_25213_Attacker3()]
+    # Call your custom attacks here
+
+    tic = time.time()
+
+    for attack in attacks:
+        attackInvoker(attack)
+        time.sleep(random.randint(1,4))
+
+    toc = time.time()
+
+    print("Total elapsed time: ", toc - tic)
+    
+
+    
+
+
+
