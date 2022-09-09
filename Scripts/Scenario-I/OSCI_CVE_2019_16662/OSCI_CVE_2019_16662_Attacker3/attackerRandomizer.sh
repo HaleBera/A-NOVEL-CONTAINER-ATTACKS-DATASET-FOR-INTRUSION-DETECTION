0b3e41d15c092c782d9ec98fc725c7d959b04b44
HBO <ogurhale@gmail.com>
2022-09-09 11:59:32 +0300

    Delete Scripts/Scenario-I directory

--- Scripts/Scenario-I/OSCI_CVE_2019_16662/OSCI_CVE_2019_16662_Attacker3/attackerRandomizer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "started processing $*.."
-sleep $((1 + RANDOM % 3)); 
-python3 attackerController.py
-echo finished processing "$*";
-
-
-# referred from: https://www.baeldung.com/linux/processing-commands-in-parallel
--- Scripts/Scenario-I/Readme.txt
+++
@@ -1,7 +0,0 @@
-Scenario I
-
-
-There 3 different attackers for each 2 unique vulnerabilities (CVE) from each weakness (CWE) category.
-
-
-- Each attacker perfoms a single flow of attack (single behavior) 
