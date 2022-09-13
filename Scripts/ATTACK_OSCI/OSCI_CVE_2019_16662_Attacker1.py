0da74e1e3dc19237d470fd91f999a842e47868d7
HBO <ogurhale@gmail.com>
2022-09-13 23:00:55 +0300

    Add files via upload

---
+++ Scripts/ATTACK_OSCI/OSCI_CVE_2019_16662_Attacker1.py
@@ -0,0 +1,30 @@
+from attackerBase import *
+from attackConfig import *
+
+
+
+class OSCI_CVE_2019_16662_Attacker1(AttackerBase):
+    
+    def __init__(self, ip_addr, yaml_path):
+        self.ip_addr = ip_addr
+        self.yaml_path = yaml_path
+        self.commandRunner = CommandRunner()
+
+    def discovery(self) -> None:
+        pass
+
+    def main(self):
+        self.commandRunner.runCommand('nuclei -u http://' + self.ip_addr +':30004  -t ' + self.yaml_path + 'YAMLs/CVE-2019-16662.yaml -silent')
+
+    def reverseShell(self):
+        pass
+       
+
+
+if __name__ == '__main__':
+    
+    # Call your custom attacks here
+    pass
+
+  
+
